2bdeb3483a9918c823ca5e36eb496e7fe2299fde
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2022-11-06 05:20:08 +0500

    setOnclickListener Event on IncBtn

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-      Button btn;
+      Button inc;
   TextView textview;
  int val=0;
     @Override
@@ -16,20 +16,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
           textview=findViewById(R.id.textview);
-          btn=findViewById(R.id.inc);
+          inc=findViewById(R.id.inc);
+
+          inc.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  String s;
+                  s = textview.getText().toString();
+                  val=Integer.parseInt(s);
+                  val++;
+                  // set text take string
+                  //or we can do it like textview.setText(Integer.toString(val));
+                  textview.setText(""+val);
+              }
+          });
 
     }
-    public  void addOne(View view){
 
-          String s;
-        s = textview.getText().toString();
-        val=Integer.parseInt(s);
-       val++;
-    // set text take string
-        //or we can do it like textview.setText(Integer.toString(val));
-        textview.setText(""+val);
 
-    }
+
+
+
     public void decOne(View view){
         String var=textview.getText().toString();
          val=Integer.parseInt(var);
