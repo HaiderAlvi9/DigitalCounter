3c77add176815727fbcb63b7d84311c5357fd818
Farooq haider alvi <farooqhaideralvi1999@gmail.com>
2022-11-08 22:44:19 +0500

    view.setListener classLevel

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,46 +7,65 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-      Button inc;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+  Button incBtn,decBtn,resBtn;
   TextView textview;
- int val=0;
+    int val=0;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+   public void onClick(View view){
+       switch (view.getId()){
+
+           case R.id.inc:
+               incOne(view);
+               break;
+
+           case R.id.dec:
+               decOne(view);
+               break;
+
+
+           case R.id.reset:
+               textview.setText("0");
+               break;
+           default:
+               break;
+       }
+   }
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
           textview=findViewById(R.id.textview);
-          inc=findViewById(R.id.inc);
+          incBtn=findViewById(R.id.inc);
+          decBtn=findViewById(R.id.dec);
+          resBtn=findViewById(R.id.reset);
+          incBtn.setOnClickListener(this);
+          decBtn.setOnClickListener(this);
+          resBtn.setOnClickListener(this);
 
-          inc.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View view) {
-                  String s;
-                  s = textview.getText().toString();
-                  val=Integer.parseInt(s);
-                  val++;
-                  // set text take string
-                  //or we can do it like textview.setText(Integer.toString(val));
-                  textview.setText(""+val);
-              }
-          });
+
+
 
     }
-
-
-
-
-
     public void decOne(View view){
         String var=textview.getText().toString();
-         val=Integer.parseInt(var);
+        val=Integer.parseInt(var);
         if(val>0){
             val=val-1;
         }
 
         textview.setText(""+val);
     }
+    public void incOne(View view){
+        String var=textview.getText().toString();
+        val=Integer.parseInt(var);
+        val=val+1;
+        textview.setText(""+val);
+    }
     public void reset(View view){
         textview.setText("0");
     }
+
 }
